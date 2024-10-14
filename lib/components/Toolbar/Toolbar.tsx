0e9d6566de42f161372e4450e4f05a3193431ffc
Nedim Kulovac <nedim.kulovac@gmail.com>
2024-10-14 10:19:05 +0200

    minor change

--- lib/components/Toolbar/Toolbar.tsx
+++ lib/components/Toolbar/Toolbar.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { ToolbarGroup } from '../ToolbarGroup';
 import { ToolbarConfig } from '../../types/toolbar';
 
@@ -7,36 +7,27 @@ type ToolbarProps = {
 };
 
 export const Toolbar = React.memo(({ config }: ToolbarProps) => {
-  const leftAlignedGroups = useMemo(
-    () =>
-      config.groups
-        .filter(
-          (group) =>
-            !group.hidden &&
-            (group.align === undefined || group.align === 'left')
-        )
-        .sort((a, b) => a.position - b.position),
-    [config.groups] // Only re-compute when groups change
-  );
+  const leftAlignedGroups = config.groups
+    .filter(
+      (group) =>
+        !group.hidden && (group.align === undefined || group.align === 'left')
+    )
+    .sort((a, b) => a.position - b.position);
 
-  const rightAlignedGroups = useMemo(
-    () =>
-      config.groups
-        .filter((group) => !group.hidden && group.align === 'right')
-        .sort((a, b) => a.position - b.position),
-    [config.groups] // Only re-compute when groups change
-  );
+  const rightAlignedGroups = config.groups
+    .filter((group) => !group.hidden && group.align === 'right')
+    .sort((a, b) => a.position - b.position);
 
   return (
-    <div className='lp-toolbar'>
-      <div className='lp-command-groups'>
+    <div className="lp-toolbar">
+      <div className="lp-command-groups">
         {leftAlignedGroups.map((group) => (
           <ToolbarGroup key={group.name} group={group} config={config} />
         ))}
       </div>
 
       {/* Right-aligned groups */}
-      <div className='lp-command-groups'>
+      <div className="lp-command-groups">
         {rightAlignedGroups.map((group) => (
           <ToolbarGroup key={group.name} group={group} config={config} />
         ))}
