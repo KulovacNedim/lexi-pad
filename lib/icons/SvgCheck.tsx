b290f905314b5ce3b8527c745b2215a3456a8171
Nedim Kulovac <nedim.kulovac@gmail.com>
2024-10-16 09:17:32 +0200

    minor change

--- lib/icons/SvgCheck.tsx
+++ lib/icons/SvgCheck.tsx
@@ -1,5 +1,5 @@
-import type { SVGProps } from "react";
-const SvgCheckSquareOffsetThinSvgrepoCom = (props: SVGProps<SVGSVGElement>) => (
+import type { SVGProps } from 'react';
+const SvgCheck = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="1em"
@@ -10,4 +10,4 @@ const SvgCheckSquareOffsetThinSvgrepoCom = (props: SVGProps<SVGSVGElement>) => (
     <path d="M216 52v152a12.013 12.013 0 0 1-12 12h-68.364a4 4 0 0 1 0-8H204a4.004 4.004 0 0 0 4-4V52a4.004 4.004 0 0 0-4-4H52a4.004 4.004 0 0 0-4 4v91.273a4 4 0 0 1-8 0V52a12.013 12.013 0 0 1 12-12h152a12.013 12.013 0 0 1 12 12m-85.172 97.172a4 4 0 0 0-5.656 0L64 210.343l-29.172-29.171a4 4 0 0 0-5.656 5.657l32 32a4 4 0 0 0 5.656 0l64-64a4 4 0 0 0 0-5.657" />
   </svg>
 );
-export default SvgCheckSquareOffsetThinSvgrepoCom;
+export default SvgCheck;
